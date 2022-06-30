07556aac1698073db0aa5d4ea172e91205c52f30
marenstockloew <marens@DESKTOP-CQBGGL9.localdomain>
2022-06-30 11:33:52 +0200

    First commit: Add existing craft website project to github.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./templates/**/*.{html,twig,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'knappe-rot': '#BF2821',
+      },
+    },
+  },
+  plugins: [],
+}
