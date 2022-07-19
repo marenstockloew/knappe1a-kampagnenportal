ed6713249a6cd92c4423743f9b0c11c3d9cd1586
marenstockloew <marens@DESKTOP-CQBGGL9.localdomain>
2022-07-19 10:59:59 +0200

    Changed font sizes and some spacings.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./templates/**/*.{html,twig,js}"],
   theme: {
+    fontSize: {
+      sm: ['13.60px', '20.57px'],
+      base: ['17.00px', '25.71px'],
+      lg: ['21.25px', '32.14px'],
+      xl: ['26.56px', '40.19px'],
+      '2xl': ['33.20px', '50.23px'],
+      '3xl': ['41.50px', '62.79px'],
+      '4xl': ['51.88px', '78.47px'],
+      '5xl': ['64.85px', '98.09px'],
+    },
     extend: {
       colors: {
         "knappe-rot": "#BF2821",
